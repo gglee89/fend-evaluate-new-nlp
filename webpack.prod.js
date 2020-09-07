b6fad6f95db109bf2abc21a75336a7c75d662f05
Gustavo Lee <gglee89@gmail.com>
2020-09-07 17:42:10 -0400

    feat: Setup webpack

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: 'client/index.js',
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./client/view/index.html",
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+}
